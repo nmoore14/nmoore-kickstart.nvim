883b119302cfd844f1f2a730fb5460aa3b6e4967
Nick Moore <nmoore@rushmarket.com>
2023-05-13 22:04:06 -0400

    Adding flutter and tweaking keymaps

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -5,11 +5,10 @@ return {
     require("catppuccin").setup({
       flavour = "frappe", -- latte, frappe, macchiato, mocha
       background = {
-                       -- :h background
         light = "latte",
         dark = "frappe",
       },
-      transparent_background = false,
+      transparent_background = true,
       show_end_of_buffer = false, -- show the '~' characters after the end of buffers
       term_colors = false,
       dim_inactive = {
@@ -42,6 +41,8 @@ return {
         telescope = true,
         notify = false,
         mini = false,
+        harpoon = true,
+        which_key = true,
         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
     })
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -45,6 +45,20 @@ vmap('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]ear
 vmap('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vmap('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 
+vmap('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+vmap('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
+
+vmap('n', 'gd', vim.lsp.buf.definition, { desc = '[G]oto [D]efinition' })
+vmap('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+vmap('n', 'gI', vim.lsp.buf.implementation, { desc = '[G]oto [I]mplementation' })
+vmap('n', '<leader>D', vim.lsp.buf.type_definition, { desc = 'Type [D]efinition' })
+vmap('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
+vmap('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols' })
+
+-- See `:help K` for why this keymap
+vmap('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
+vmap('n', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation' })
+
 -- Diagnostic keymaps
 vmap('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vmap('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
@@ -78,3 +92,6 @@ vmap("n", "<leader>a1", function() ui.nav_file(1) end)
 vmap("n", "<leader>a2", function() ui.nav_file(2) end)
 vmap("n", "<leader>a3", function() ui.nav_file(3) end)
 vmap("n", "<leader>a4", function() ui.nav_file(4) end)
+
+-- Flutter-tools Keymaps
+vmap('n', '<leader>fo', '<cmd>FlutterOutlineToggle<CR>', options)
