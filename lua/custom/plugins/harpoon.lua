9b51757464c87b74361bcba27b7e5cce72ed510e
Nick Moore <nmoore@rushmarket.com>
2023-05-17 11:29:41 -0400

    Changing harpoon width and theme

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -3,7 +3,7 @@ return {
   config = function ()
     require('harpoon').setup({
       menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
+        width = vim.api.nvim_win_get_width(0) - 40,
       }
     })
   end
--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,51 +1,31 @@
 return {
-  'catppuccin/nvim',
+  'rebelot/kanagawa.nvim',
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavour = "frappe", -- latte, frappe, macchiato, mocha
-      background = {
-        light = "latte",
-        dark = "frappe",
+    require('kanagawa').setup({
+      compile = false,             -- enable compiling the colorscheme
+      undercurl = true,            -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true},
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = true,         -- do not set background color
+      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+      colors = {                   -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
       },
-      transparent_background = true,
-      show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-      term_colors = false,
-      dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
-      color_overrides = {},
-      custom_highlights = {},
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        notify = false,
-        mini = false,
-        harpoon = true,
-        which_key = true,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      overrides = function(colors) -- add/modify highlights
+          return {}
+      end,
+      theme = "wave",              -- Load "wave" theme when 'background' option is not set
+      background = {               -- map the value of 'background' option to a theme
+          dark = "wave",           -- try "dragon" !
+          light = "lotus"
       },
     })
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'kanagawa'
   end,
 }
