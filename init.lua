3a933a7356a480cf95801eabe9842c552ad77c56
Nick Moore <nmoore@rushmarket.com>
2023-07-06 12:12:55 -0400

    Adding more options to nvim-treesitter

--- init.lua
+++ init.lua
@@ -112,9 +112,31 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/nvim-treesitter-refactor',
+      'nvim-treesitter/playground'
     },
     config = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
+      require "nvim-treesitter.configs".setup {
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+          },
+        }
+      }
     end,
   },
   { 'mbbill/undotree' },
