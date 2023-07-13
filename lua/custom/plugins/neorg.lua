66b2dbc218b956d31afee20f529ab10cadebf8c5
Nick Moore <nmoore@rushmarket.com>
2023-07-12 23:10:42 -0400

    Tweaking neorg config and adding zen-mode for neorg

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -1,26 +1,47 @@
- return {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.completion"] = {},
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/Desktop/_nick/org/notes",
-                projects = "~/Desktop/_nick/org/projects",
-                work = "~/Desktop/_nick/org/work",
-              },
-            },
-           ["core.presenter"] = {},
-           ["core.summary"] = {},
-           ["core.ui.calendar"] = {}
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup {
+      load = {
+        ["core.integrations.treesitter"] = {
+          config = {
+            configure_parsers = true,
+            intstall_parsers = true,
           },
         },
-      }
-    end,
-  }
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          }
+        },
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/Desktop/_nick/org/notes",
+              projects = "~/Desktop/_nick/org/projects",
+              work = "~/Desktop/_nick/org/work",
+            },
+          },
+        },
+        ["core.keybinds"] = {},
+        ["core.presenter"] = {
+          config = {
+             zen_mode = "zen-mode",
+          },
+        },
+        ["core.qol.todo_items"] = {
+          config = {
+            create_todo_items = true,
+            create_todo_parents = true,
+          },
+        },
+        ["core.summary"] = {},
+        ["core.ui.calendar"] = {}
+      },
+    }
+  end,
+}
