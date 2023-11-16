c8ce956871f783816d7f0f677cba5496cf5aa161
Nick Moore <nickmoore_1@live.com>
2023-11-16 13:08:18 -0500

    Adding todo comment, changing themes, and fixing noice config

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -7,6 +7,10 @@ return {
     "rcarriga/nvim-notify",
   },
   config = function ()
+    require("notify").setup({
+      background_colour = "#000000",
+    })
+
     require("noice").setup({
       cmdline = {
         enabled = true, -- enables the Noice cmdline UI
