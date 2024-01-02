fc1843fe60953810e8db075568ffe0e15af2c4e3
Nick Moore <nickmoore_1@live.com>
2024-01-01 21:08:35 -0500

    Updating theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,60 +1,46 @@
 return {
-  "Mofiqul/dracula.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+  'ray-x/starry.nvim',
   config = function ()
-    require("dracula").setup({
-      -- customize dracula color palette
-      colors = {
-        bg = "#282A36",
-        fg = "#F8F8F2",
-        selection = "#44475A",
-        comment = "#6272A4",
-        red = "#FF5555",
-        orange = "#FFB86C",
-        yellow = "#F1FA8C",
-        green = "#50fa7b",
-        purple = "#BD93F9",
-        cyan = "#8BE9FD",
-        pink = "#FF79C6",
-        bright_red = "#FF6E6E",
-        bright_green = "#69FF94",
-        bright_yellow = "#FFFFA5",
-        bright_blue = "#D6ACFF",
-        bright_magenta = "#FF92DF",
-        bright_cyan = "#A4FFFF",
-        bright_white = "#FFFFFF",
-        menu = "#21222C",
-        visual = "#3E4452",
-        gutter_fg = "#4B5263",
-        nontext = "#3B4048",
-        white = "#ABB2BF",
-        black = "#191A21",
+    require('starry').setup({
+      border = false, -- Split window borders
+      italics = {
+        comments = true, -- Italic comments
+        strings = false, -- Italic strings
+        keywords = true, -- Italic keywords
+        functions = false, -- Italic functions
+        variables = false -- Italic variables
       },
-      -- show the '~' characters after the end of buffers
-      show_end_of_buffer = true, -- default false
-      -- use transparent background
-      transparent_bg = true, -- default false
-      -- set custom lualine background color
-      lualine_bg_color = "#44475a", -- default nil
-      -- set italic comment
-      italic_comment = true, -- default false
-      -- overrides the default highlights with table see `:h synIDattr`
-      overrides = {},
-      -- You can use overrides as table like this
-      -- overrides = {
-      --   NonText = { fg = "white" }, -- set NonText fg to white
-      --   NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
-      --   Nothing = {} -- clear highlight of Nothing
-      -- },
-      -- Or you can also use it like a function to get color from theme
-      -- overrides = function (colors)
-      --   return {
-      --     NonText = { fg = colors.white }, -- set NonText fg to white of theme
-      --   }
-      -- end,
+      contrast = { -- Select which windows get the contrast background
+        enable = true, -- Enable contrast
+        terminal = true, -- Darker terminal
+        filetypes = {}, -- Which filetypes get darker? e.g. *.vim, *.cpp, etc.
+      },
+      text_contrast = {
+        lighter = false, -- Higher contrast text for lighter style
+        darker = false -- Higher contrast text for darker style
+      },
+      disable = {
+        background = false, -- true: transparent background
+        term_colors = false, -- Disable setting the terminal colors
+        eob_lines = false -- Make end-of-buffer lines invisible
+      },
+      style = {
+        name = 'mariana', -- Theme style name (moonlight, earliestsummer, etc.)
+        -- " other themes: dracula, oceanic, dracula_blood, 'deep ocean', darker, palenight, monokai, mariana, emerald, middlenight_blue
+        disable = {},  -- a list of styles to disable, e.g. {'bold', 'underline'}
+        fix = true,
+        darker_contrast = false, -- More contrast for darker style
+        daylight_swith = false, -- Enable day and night style switching
+        deep_black = false, -- Enable a deeper black background
+      },
+      custom_colors = {
+        variable = '#f797d7',
+      },
+      custom_highlights = {
+        LineNr = { fg = '#777777' },
+        Idnetifier = { fg = '#ff4797' },
+      }
     })
-    vim.cmd[[colorscheme dracula-soft]]
+    vim.cmd[[colorscheme starry]]
   end
 }
