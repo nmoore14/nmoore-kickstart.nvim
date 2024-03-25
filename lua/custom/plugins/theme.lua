a2edca89c73fba84c815eb5e31d68c24b2d4b47c
Nick Moore <nickmoore_1@live.com>
2024-03-25 18:34:13 -0400

    [updates] Adding colorizer and updating my theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,55 +1,24 @@
 return {
-  'AlexvZyl/nordic.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('nordic').setup {
-      -- This callback can be used to override the colors used in the palette.
-      on_palette = function(palette)
-        return palette
-      end,
-      -- Enable bold keywords.
-      bold_keywords = false,
-      -- Enable italic comments.
-      italic_comments = true,
-      -- Enable general editor background transparency.
-      transparent_bg = false,
-      -- Enable brighter float border.
-      bright_border = false,
-      -- Reduce the overall amount of blue in the theme (diverges from base Nord).
-      reduced_blue = true,
-      -- Swap the dark background with the normal one.
-      swap_backgrounds = false,
-      -- Override the styling of any highlight group.
-      override = {},
-      -- Cursorline options.  Also includes visual/selection.
-      cursorline = {
-        -- Bold font in cursorline.
-        bold = false,
-        -- Bold cursorline number.
-        bold_number = true,
-        -- Available styles: 'dark', 'light'.
-        theme = 'dark',
-        -- Blending the cursorline bg with the buffer bg.
-        blend = 0.85,
-      },
-      noice = {
-        -- Available styles: `classic`, `flat`.
-        style = 'classic',
-      },
-      telescope = {
-        -- Available styles: `classic`, `flat`.
-        style = 'flat',
-      },
-      leap = {
-        -- Dims the backdrop when using leap.
-        dim_backdrop = false,
-      },
-      ts_context = {
-        -- Enables dark background for treesitter-context window
-        dark_background = true,
-      },
-    }
-    vim.cmd.colorscheme 'nordic'
-  end,
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
+  config = function ()
+    require("onedarkpro").setup({
+      styles = {
+        types = "NONE",
+        methods = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        comments = "italic",
+        keywords = "bold,italic",
+        constants = "NONE",
+        functions = "italic",
+        operators = "NONE",
+        variables = "NONE",
+        parameters = "NONE",
+        conditionals = "NONE",
+        virtual_text = "NONE",
+      }
+    })
+    vim.cmd("colorscheme onedark")
+  end
 }
